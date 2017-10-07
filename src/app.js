e3a318d73844764ac949c8dcd58791a8e11b1840
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-10-07 15:35:10 -0300

    Incluindo views

--- src/app.js
+++ src/app.js
@@ -5,9 +5,24 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/tecnologia', function(req, res){
-    res.render("../views/sessao/tecnologia");
+
+app.get('/', function(req, res){
+    res.render("../views/home/index");
 });
+
+app.get('/admin/add', function(req, res){
+    res.render("../views/admin/add_noticia");
+});
+
+app.get('/noticias/', function(req, res){
+    res.render("../views/noticias/noticias");
+});
+
+app.get('/noticias/detalhes', function(req, res){
+    res.render("../views/noticias/detalhe");
+});
+
+
 
 app.listen(3000, function(){
     console.log('Rodando com express');
